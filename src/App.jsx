da68789e36adbfcee0407c4f904d6816acfaa56b
TanmayNileshShinde <tanmaynileshshinde@gmail.com>
2026-04-11 02:10:31 +0530

    added dududu

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const MY_FILES = [
-  "madmax.ipynb"
+  "madmax.ipynb",
   "dududu.ipynb"
 ];
 
